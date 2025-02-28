31ac3e671152a539c074bb865d70213ed4ad1032
pannatheszucs <szpa23at@student.ju.se>
2025-02-28 08:54:13 +0100

    event listener

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const buttonElement = document.getElementById("submit");
 
 buttonElement.addEventListener("click", function (e) {
   e.preventDefault();
-  headerElement.innerText = "your message was sent! send another?";
-  localStorage.text = "your message was sent! send another?";
+  if (messageInput.value !== "") {
+    headerElement.innerText = "your message was sent! send another?";
+    localStorage.text = "your message was sent! send another?";
+  }
 });
+
+if (localStorage.text) {
+  headerElement.innerText = "your message was sent! send another?";
+}
